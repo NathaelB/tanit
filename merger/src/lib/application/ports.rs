5027b77c361bb99c9d53e6691f8b0bb9be5a893f
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2025-01-13 18:41:56 +0100

    feat: Add merger module and dependencies

---
+++ merger/src/lib/application/ports.rs
@@ -0,0 +1,22 @@
+use std::{fmt::Debug, future::Future};
+
+use serde::de::DeserializeOwned;
+
+pub trait MessagingPort: Clone + Send + Sync + 'static {
+    fn publish_message(
+        &self,
+        topic: String,
+        message: String,
+    ) -> impl Future<Output = anyhow::Result<()>> + Send;
+
+    fn subscribe<F, T, Fut>(
+        &self,
+        topic: &str,
+        group_id: &str,
+        handler: F,
+    ) -> impl Future<Output = anyhow::Result<()>> + Send
+    where
+        F: Fn(T) -> Fut + Send + Sync + 'static,
+        Fut: Future<Output = anyhow::Result<()>> + Send + 'static,
+        T: DeserializeOwned + Send + Sync + Debug + Clone + 'static;
+}
