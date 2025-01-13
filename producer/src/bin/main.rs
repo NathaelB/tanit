ddd187ebbbe20c638d274a89db4177fd33dcfd32
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2025-01-13 16:04:27 +0100

    feat: Add HTTP server with Tanit framework
    This commit adds an HTTP server implementation using the Tanit framework. It sets up a server on port 3333 and runs it asynchronously using the `tokio::main` attribute. The server is configured with the `HttpServerConfig` and starts running by calling the `run` method on the `HttpServer` instance.
    
    The purpose of this change is to enable the application to handle HTTP requests and responses.

--- producer/src/bin/main.rs
+++ producer/src/bin/main.rs
@@ -1,3 +1,14 @@
-fn main() {
+use anyhow::Result;
+use tanit::application::http::{HttpServer, HttpServerConfig};
+
+#[tokio::main]
+async fn main() -> Result<()> {
     println!("Hello, world!");
+
+    let server_config = HttpServerConfig::new("3333".to_string());
+    let http_server = HttpServer::new(server_config).await?;
+
+    http_server.run().await?;
+
+    Ok(())
 }
