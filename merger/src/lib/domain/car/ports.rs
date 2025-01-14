30b9cbd10cfe2dda527bcfd43bcac16fa69a8796
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2025-01-14 14:11:28 +0100

    refactor: Update car module to adhere to Rust naming conventions
    The code changes in the car module update the struct fields to follow the Rust naming conventions. The fields `license_plate`, `brand`, `color`, and `capacity` are now indented with four spaces instead of two. This change improves code readability and consistency.

--- merger/src/lib/domain/car/ports.rs
+++ merger/src/lib/domain/car/ports.rs
@@ -2,11 +2,10 @@ use std::future::Future;
 
 use super::models::{Car, CarError};
 
-
 pub trait CarService: Clone + Send + Sync + 'static {
-  fn create(&self, car: Car) -> impl Future<Output = Result<Car, CarError>> + Send;
+    fn create(&self, car: Car) -> impl Future<Output = Result<Car, CarError>> + Send;
 }
 
 pub trait CarRepository: Clone + Send + Sync + 'static {
-  fn create(&self, car: Car) -> impl Future<Output = Result<Car, CarError>> + Send;
+    fn create(&self, car: Car) -> impl Future<Output = Result<Car, CarError>> + Send;
 }
--- merger/src/lib/domain/car/service.rs
+++ merger/src/lib/domain/car/service.rs
@@ -1,32 +1,30 @@
-use super::{models::{Car, CarError}, ports::{CarRepository, CarService}};
-
+use super::{
+    models::{Car, CarError},
+    ports::{CarRepository, CarService},
+};
 
 #[derive(Clone)]
-pub struct CarServiceImpl<C> 
+pub struct CarServiceImpl<C>
 where
-  C: CarRepository
+    C: CarRepository,
 {
-  pub car_repository: C
+    pub car_repository: C,
 }
 
-impl<C> CarServiceImpl<C> 
-where 
-  C: CarRepository
+impl<C> CarServiceImpl<C>
+where
+    C: CarRepository,
 {
-  pub fn new(car_repository: C) -> Self {
-    Self {
-      car_repository
+    pub fn new(car_repository: C) -> Self {
+        Self { car_repository }
     }
-  }
 }
 
-
-impl<C> CarService for CarServiceImpl<C> 
-where 
-  C: CarRepository
+impl<C> CarService for CarServiceImpl<C>
+where
+    C: CarRepository,
 {
-  async fn create(&self, car: Car) -> Result<Car, CarError> {
-      todo!()
-  }
+    async fn create(&self, _car: Car) -> Result<Car, CarError> {
+        todo!()
+    }
 }
-
