c14bc65f889a82ea2bf0d2ff69105484990fe34f
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2025-01-15 17:58:22 +0100

    feat: Refactor code to use FerryService for creating Ferries
    The code changes introduce a new `FerryService` trait and a corresponding implementation `FerryServiceImpl`. The `FerryService` trait defines a `create` method for creating Ferries with a specified capacity. This refactoring allows for better separation of concerns and improves the extensibility of the codebase.

--- merger/src/lib/infrastructure/repositories/in_memory_car_repository.rs
+++ merger/src/lib/infrastructure/repositories/in_memory_car_repository.rs
@@ -1,1 +1,26 @@
+use std::{collections::HashMap, sync::{Arc, Mutex}};
 
+use crate::domain::car::{models::{Car, CarError}, ports::CarRepository};
+
+
+#[derive(Clone, Default)]
+pub struct InMemoryCarRepository {
+  cars: Arc<Mutex<HashMap<String, Car>>>
+}
+
+impl CarRepository for InMemoryCarRepository {
+  async fn create(&self, car: Car, ferry_id: String) -> Result<Car, CarError> {
+      let mut cars = self.cars.lock().unwrap();
+      cars.insert(ferry_id, car.clone());
+
+      Ok(car)
+  }
+
+  async fn delete_by_ferry_id(&self, id: &str) -> Result<(), CarError> {
+      let mut cars = self.cars.lock().unwrap();
+
+      cars.remove(id);
+
+      Ok(())
+  }
+}
