ed15b64a2568a8da13f112c1119c8789f042767f
Thomas <broine.thomas@gmail.com>
2025-01-15 16:14:11 +0100

    feat: Add passenger handling and refactor ferry service methods

--- merger/src/lib/domain/passenger/models.rs
+++ merger/src/lib/domain/passenger/models.rs
@@ -1,1 +1,22 @@
+use serde::Deserialize;
 
+#[derive(Debug, Clone)]
+pub struct Passenger {
+    pub id: String,
+    pub name: String,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct CreatePassengerEvent {
+    pub id: String,
+    pub name: String,
+}
+
+impl Passenger {
+    pub fn from_event(event: CreatePassengerEvent) -> Self {
+        Passenger {
+            id: event.id,
+            name: event.name,
+        }
+    }
+}
--- merger/src/lib/domain/passenger/ports.rs
+++ merger/src/lib/domain/passenger/ports.rs
@@ -1,1 +1,11 @@
+use std::future::Future;
 
+use super::models::Passenger;
+
+pub trait PassengerService: Clone + Send + Sync + 'static {
+    fn add_passenger(&self, passenger: Passenger) -> impl Future<Output = anyhow::Result<()>> + Send;
+}
+
+pub trait PassengerRepository: Clone + Send + Sync + 'static {
+    fn save(&self, passenger: Passenger) -> impl Future<Output = anyhow::Result<()>> + Send;
+}
