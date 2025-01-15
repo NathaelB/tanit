52a64226640e836cfd33799091321f773afcc5f6
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2025-01-15 12:09:51 +0100

    feat: Refactor code to use FerryService for creating Ferries
    The code changes introduce a new `FerryService` trait and a corresponding implementation `FerryServiceImpl`. The `FerryService` trait defines a `create` method for creating Ferries with a specified capacity. The `FerryServiceImpl` implementation generates a unique ID for each Ferry and assigns a random name using the `CompanyName` faker. This refactoring allows for better separation of concerns and improves the extensibility of the codebase.
    
    Co-authored-by: Giada De Martino <giada-de-martino@etu.umontpellier.fr>
    Co-authored-by: Isalyne <isalyne.llinares-rames@etu.umontpellier.fr>

---
+++ producer/src/lib/domain/ports.rs
@@ -0,0 +1,9 @@
+use std::future::Future;
+
+use anyhow::Result;
+
+use super::models::Ferry;
+
+pub trait FerryService: Clone + Send + Sync + 'static {
+  fn create(&self, capacity: i32) -> impl Future<Output = Result<Ferry>> + Send;
+}
