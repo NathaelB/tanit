a7ad0929e6960e3d173ad289b9cfbeb4998b245b
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2025-01-15 17:58:22 +0100

    feat: Refactor code to use FerryService for creating Ferries
    The code changes introduce a new `FerryService` trait and a corresponding implementation `FerryServiceImpl`. The `FerryService` trait defines a `create` method for creating Ferries with a specified capacity. This refactoring allows for better separation of concerns and improves the extensibility of the codebase.

--- merger/src/lib/infrastructure/repositories/in_memory_passenger_repository.rs
+++ merger/src/lib/infrastructure/repositories/in_memory_passenger_repository.rs
@@ -1,1 +1,19 @@
+use std::{collections::HashMap, sync::{Arc, Mutex}};
 
+use crate::domain::passenger::{models::Passenger, ports::PassengerRepository};
+
+#[derive(Clone, Default)]
+pub struct InMemoryPassengerRepository {
+  pub passengers: Arc<Mutex<HashMap<String, Vec<Passenger>>>>
+}
+
+impl PassengerRepository for InMemoryPassengerRepository {
+  async fn save(&self, passenger: Passenger) -> anyhow::Result<()> {
+      let mut passengers = self.passengers.lock().unwrap();
+      let ferry_id = passenger.ferry_id.clone();
+      let passenger_list = passengers.entry(ferry_id).or_insert(Vec::new());
+      passenger_list.push(passenger.clone());
+
+      Ok(())
+  }
+}
