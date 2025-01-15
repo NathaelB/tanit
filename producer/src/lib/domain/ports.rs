eb410fe17298f9b4dcc03506694af58bb229b238
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2025-01-15 18:13:34 +0100

    feat: Refactor dataset service to use dependency injection
    The code changes refactor the dataset service to use dependency injection for the ferry, car, and passenger services. This improves the modularity and testability of the code by decoupling the dataset service from its dependencies. The dataset service now accepts the necessary services as constructor arguments, allowing for easier swapping of implementations and mocking during testing.

--- producer/src/lib/domain/ports.rs
+++ producer/src/lib/domain/ports.rs
@@ -1,18 +1,7 @@
+use super::models::DataSet;
 use anyhow::Result;
 use std::future::Future;
-use std::sync::Arc;
-
-use super::car::ports::CarService;
-use super::ferry::ports::FerryService;
-use super::models::DataSet;
-use super::passenger::ports::PassengerService;
 
 pub trait DataSetService: Clone + Send + Sync + 'static {
-    fn create_data_set<F: FerryService, C: CarService, P: PassengerService>(
-        &self,
-        capacity_ferry: i32,
-        ferry_service: Arc<F>,
-        car_service: Arc<C>,
-        passenger_service: Arc<P>,
-    ) -> impl Future<Output = Result<DataSet>> + Send;
+    fn create_data_set(&self, capacity_ferry: i32) -> impl Future<Output = Result<DataSet>> + Send;
 }
