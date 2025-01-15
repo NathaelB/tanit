781fb2ee927679ddcdff83ecc5a4093fff2aff38
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2025-01-15 11:34:47 +0100

    feat: Add messaging module for Avro schema registration
    The code changes add a new module called "messaging" under the "application" module. This module includes functions for creating Avro schemas for different entities such as "Ferry", "Car", and "Passenger". Each schema is defined as a JSON string and registered with the schema registry using the provided settings. This change enables the application to dynamically register and use Avro schemas for messaging.
    
    Co-authored-by: Giada De Martino <giada-de-martino@etu.umontpellier.fr>
    Co-authored-by: Isalyne <isalyne.llinares-rames@etu.umontpellier.fr>

---
+++ producer/src/lib/application/messaging.rs
@@ -0,0 +1,115 @@
+use anyhow::Result;
+use schema_registry_converter::{
+    async_impl::schema_registry::{post_schema, SrSettings},
+    schema_registry_common::{SchemaType, SuppliedSchema},
+};
+
+pub async fn create_ferri_schema() -> Result<()> {
+    let schema_str = r#"
+  {
+      "type": "record",
+      "name": "Ferry",
+      "namespace": "nl.openweb.data",
+      "fields": [
+          {"name": "id", "type": "string"},
+          {"name": "name", "type": "string"},
+          {"name": "capacity", "type": "int"}
+      ]
+  }
+  "#;
+
+    let supplied_schema = SuppliedSchema {
+        name: Some(String::from("nl.openweb.data.Ferry")),
+        schema_type: SchemaType::Avro,
+        schema: String::from(schema_str),
+        references: vec![],
+    };
+
+    let sr_settings = SrSettings::new("http://localhost:8081".to_string());
+
+    let registed_schema = post_schema(
+        &sr_settings,
+        "ferries-value".to_string(),
+        supplied_schema.clone(),
+    )
+    .await?;
+
+    println!("Schema registered with ID: {}", registed_schema.id);
+
+    Ok(())
+}
+
+pub async fn create_car_schema() -> Result<()> {
+    let schema_str = r#"
+  {
+      "type": "record",
+      "name": "Car",
+      "namespace": "nl.openweb.data",
+      "fields": [
+          {"name": "id", "type": "string"},
+          {"name": "licence_plate", "type": "string"},
+          {"name": "brand", "type": "string"},
+          {"name": "color", "type": "string"},
+          {"name": "capacity", "type": "int"}
+      ]
+  }
+  "#;
+
+    let supplied_schema = SuppliedSchema {
+        name: Some(String::from("nl.openweb.data.Car")),
+        schema_type: SchemaType::Avro,
+        schema: String::from(schema_str),
+        references: vec![],
+    };
+
+    let sr_settings = SrSettings::new("http://localhost:8081".to_string());
+
+    let registed_schema = post_schema(
+        &sr_settings,
+        "cars-value".to_string(),
+        supplied_schema.clone(),
+    )
+    .await?;
+
+    println!("Schema registered with ID: {}", registed_schema.id);
+
+    Ok(())
+}
+
+
+pub async fn create_passernger_schema() -> Result<()> {
+  let schema_str = r#"
+  {
+      "type": "record",
+      "name": "Passenger",
+      "namespace": "nl.openweb.data",
+      "fields": [
+          {"name": "id", "type": "string"},
+          {"name": "car_id", "type": ["null", "string"]},
+          {"name": "ferry_id", "type": "string"},
+          {"name": "firstname", "type": "string"},
+          {"name": "lastname", "type": "string"},
+          {"name": "sex", "type": "boolean"}
+      ]
+  }
+  "#;
+
+    let supplied_schema = SuppliedSchema {
+        name: Some(String::from("nl.openweb.data.Passenger")),
+        schema_type: SchemaType::Avro,
+        schema: String::from(schema_str),
+        references: vec![],
+    };
+
+    let sr_settings = SrSettings::new("http://localhost:8081".to_string());
+
+    let registed_schema = post_schema(
+        &sr_settings,
+        "passengers-value".to_string(),
+        supplied_schema.clone(),
+    )
+    .await?;
+
+    println!("Schema registered with ID: {}", registed_schema.id);
+  Ok(())
+}
