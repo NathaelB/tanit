885b80b13f5ab1174469f4d078e93036bddf707b
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2025-01-16 14:58:05 +0100

    chore: Update Kafka dependencies and configuration
    This commit updates the Kafka dependencies in the Cargo.toml file to include the latest versions of `apache-avro` and `schema_registry_converter`. It also modifies the Kafka initialization and subscription parameters in the `Kafka` struct to improve the configuration. These changes ensure compatibility with the latest Kafka libraries and enhance the performance and functionality of the messaging module.
    
    Co-authored-by: Giada De Martino <giada-de-martino@etu.umontpellier.fr>
    Co-authored-by: Isalyne <isalyne.llinares-rames@etu.umontpellier.fr>

--- saver/src/lib/infrastructure/messaging/kafka.rs
+++ saver/src/lib/infrastructure/messaging/kafka.rs
@@ -1,36 +1,27 @@
 use crate::application::ports::{MessagingPort, Offset, SubscriptionOptions};
 use anyhow::Result;
+use apache_avro::from_value;
 use futures::StreamExt;
-use rdkafka::{
-    consumer::{Consumer, StreamConsumer},
-    producer::FutureProducer,
-    ClientConfig, Message,
-};
+use rdkafka::{consumer::{Consumer, StreamConsumer}, producer::FutureProducer, ClientConfig, Message};
+use schema_registry_converter::async_impl::{avro::AvroDecoder, schema_registry::SrSettings};
 use std::{collections::HashMap, sync::Arc};
 
 #[derive(Clone)]
 pub struct Kafka {
     #[allow(unused)]
     producer: Arc<FutureProducer>,
-    consumer: Arc<StreamConsumer>,
+    brokers: String,
 }
 
 impl Kafka {
-    pub fn new(brokers: String, group_id: String) -> Result<Self> {
+    pub fn new(brokers: String, _group_id: String) -> Result<Self> {
         let producer = ClientConfig::new()
             .set("bootstrap.servers", &brokers)
             .create::<FutureProducer>()?;
 
-        let consumer = ClientConfig::new()
-            .set("bootstrap.servers", &brokers)
-            .set("group.id", &group_id)
-            .set("enable.auto.commit", "true")
-            .set("auto.offset.reset", "earliest")
-            .create::<StreamConsumer>()?;
-
         Ok(Kafka {
             producer: Arc::new(producer),
-            consumer: Arc::new(consumer),
+            brokers,
         })
     }
 }
@@ -52,25 +43,42 @@ impl MessagingPort for Kafka {
         Fut: std::future::Future<Output = anyhow::Result<()>> + Send + 'static,
         T: serde::de::DeserializeOwned + Send + Sync + std::fmt::Debug + Clone + 'static,
     {
-        self.consumer.subscribe(&[topic])?;
+        let consumer = ClientConfig::new()
+            .set("bootstrap.servers", &self.brokers)
+            .set("group.id", "merger")
+            .set("enable.auto.commit", "true")
+            .set("auto.offset.reset", "earliest")
+            .create::<StreamConsumer>()?;
 
-        if let Offset::Beginning = options.offset {
-            let mut hash_map = HashMap::new();
-            hash_map.insert((topic.to_string(), 0), rdkafka::Offset::Beginning);
-            let t = rdkafka::TopicPartitionList::from_topic_map(&hash_map).unwrap();
-            self.consumer.assign(&t)?;
-        }
+        // if let Offset::Beginning = options.offset {
+        //     let mut hash_map = HashMap::new();
+        //     hash_map.insert((topic.to_string(), 0), rdkafka::Offset::Beginning);
+        //     let t = rdkafka::TopicPartitionList::from_topic_map(&hash_map).unwrap();
+        //     consumer.assign(&t)?;
+        // }
 
-        let consumer: Arc<StreamConsumer> = Arc::clone(&self.consumer);
+        consumer.subscribe(&[topic])?;
+
+        let sr_settings = SrSettings::new("http://localhost:8081".to_string());
+        let avro_decoder = AvroDecoder::new(sr_settings);
 
         tokio::spawn(async move {
             while let Some(result) = consumer.stream().next().await {
                 match result {
                     Ok(message) => {
-                        if let Some(payload) = message.payload_view::<str>() {
+                        if let Some(payload) = message.payload_view::<[u8]>() {
                             match payload {
-                                Ok(text) => {
-                                    let parsed_message: T = match serde_json::from_str(text) {
+                                Ok(bytes) => {
+                                    let decoded =
+                                        match avro_decoder.decode_with_schema(Some(bytes)).await {
+                                            Ok(decoded) => decoded,
+                                            Err(e) => {
+                                                eprintln!("Error while decoding message: {:?}", e);
+                                                continue;
+                                            }
+                                        }
+                                        .unwrap();
+                                    let parsed_message: T = match from_value(&decoded.value) {
                                         Ok(msg) => msg,
                                         Err(e) => {
                                             eprintln!("Failed to parse message: {:?}", e);
